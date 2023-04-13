5d1c26f7b1f8e46ba8674fe8f037badfd6d8df49
Sean Kelly <kelly@seankelly.biz>
2023-04-13 10:08:23 -0500

    Support for TLS and SSL

--- src/datascience/settings/base.py
+++ src/datascience/settings/base.py
@@ -225,3 +225,5 @@ EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.jpl.nasa.gov')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
